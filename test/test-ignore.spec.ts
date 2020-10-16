0765969eb9478d3b07978d8c54f27baed4e9e676
Pavel <pavel.feldman@gmail.com>
2020-10-16 13:47:39 -0700

    feat(gitignore): respect ignores in gitignore

--- test/test-ignore.spec.ts
+++ test/test-ignore.spec.ts
@@ -17,25 +17,57 @@
 import { folio } from './fixtures';
 const { it, expect } = folio;
 
-it('should run all three tests', async ({ runTest }) => {
-  const result = await runTest('test-ignore');
+const tests = {
+  'a.test.ts': `
+    it('pass', ({}) => {});
+  `,
+  'b.test.ts': `
+    it('pass', ({}) => {});
+  `,
+  'c.test.ts': `
+    it('pass', ({}) => {});
+  `
+};
+
+it('should run all three tests', async ({ runInlineTest }) => {
+  const result = await runInlineTest(tests);
   expect(result.passed).toBe(3);
   expect(result.exitCode).toBe(0);
 });
 
-it('should ignore a test', async ({ runTest }) => {
-  const result = await runTest('test-ignore', { 'test-ignore': 'b.test.ts' });
+it('should ignore a test', async ({ runInlineTest }) => {
+  const result = await runInlineTest(tests, { 'test-ignore': 'b.test.ts' });
   expect(result.passed).toBe(2);
   expect(result.exitCode).toBe(0);
 });
 
-it('should filter tests', async ({ runTest }) => {
-  const result = await runTest('test-ignore', { 'test-match': 'c.test.*' });
+it('should ignore a folder', async ({ runInlineTest }) => {
+  const result = await runInlineTest({
+    'a.test.ts': `
+      it('pass', ({}) => {});
+    `,
+    'folder/a.test.ts': `
+      it('pass', ({}) => {});
+    `,
+    'folder/b.test.ts': `
+      it('pass', ({}) => {});
+    `,
+    'folder/c.test.ts': `
+      it('pass', ({}) => {});
+    `
+  }, { 'test-ignore': 'folder/**' });
   expect(result.passed).toBe(1);
+  expect(result.exitCode).toBe(0);
 });
 
-it('should use a different test match', async ({ runTest }) => {
-  const result = await runTest('test-ignore', { 'test-match': '[a|b].test.ts' });
+it('should filter tests', async ({ runInlineTest }) => {
+  const result = await runInlineTest(tests, { 'test-ignore': 'c.test.*' });
+  expect(result.passed).toBe(2);
+  expect(result.exitCode).toBe(0);
+});
+
+it('should use a different test match', async ({ runInlineTest }) => {
+  const result = await runInlineTest(tests, { 'test-match': '[a|b].test.ts' });
   expect(result.passed).toBe(2);
   expect(result.exitCode).toBe(0);
 });
