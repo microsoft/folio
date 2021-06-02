cd0c8a0aa627179ab8a286019193f76fdd0f4bbc
Dmitry Gozman <dgozman@gmail.com>
2021-06-02 16:25:54 -0700

    fix(cli): fix some typos and help

--- src/cli.ts
+++ src/cli.ts
@@ -24,6 +24,8 @@ import { forceRegExp } from './util';
 const defaultTimeout = 10000;
 const defaultReporter = process.env.CI ? 'dot' : 'list';
 const builtinReporters = ['list', 'line', 'dot', 'json', 'junit', 'null'];
+const tsConfig = 'folio.config.ts';
+const jsConfig = 'folio.config.js';
 const defaultConfig: Config = {
   preserveOutput: process.env.CI ? 'failures-only' : 'always',
   reporter: [defaultReporter],
@@ -34,27 +36,26 @@ const defaultConfig: Config = {
 
 const program = new commander.Command();
 program.name('folio');
-program.usage('[filter...] [options]');
-program.description('Use [filter...] arguments to filter test files. Each argument is treated as a regular expression.');
+program.arguments('[test-filter...]');
 program.helpOption(false);
 program.option('-h, --help', `Display help`);
-program.option('-c, --config <file>', `Configuration file (default: "folio.config.ts" or "folio.config.js")`);
-program.option('--forbid-only', `Fail if exclusive test(s) encountered (default: ${defaultConfig.forbidOnly})`);
-program.option('-g, --grep <grep>', `Only run tests matching this regular expression (default: "${defaultConfig.grep}")`);
-program.option('--global-timeout <timeout>', `Maximum time this test suite can run in milliseconds (default: 0 for unlimited)`);
-program.option('-j, --workers <workers>', `Number of concurrent workers, use 1 to run in single worker (default: number of CPU cores / 2)`);
+program.option('-c, --config <file>', `Configuration file, or a test directory with optional "${tsConfig}"/"${jsConfig}"`);
+program.option('--forbid-only', `Fail if test.only is called (default: false)`);
+program.option('-g, --grep <grep>', `Only run tests matching this regular expression (default: ".*")`);
+program.option('--global-timeout <timeout>', `Maximum time this test suite can run in milliseconds (default: unlimited)`);
+program.option('-j, --workers <workers>', `Number of concurrent workers, use 1 to run in a single worker (default: number of CPU cores / 2)`);
 program.option('--list', `Collect all the tests and report them, but do not run`);
-program.option('--max-failures <N>', `Stop after the first N failures (default: do not stop until all tests are run)`);
+program.option('--max-failures <N>', `Stop after the first N failures`);
 program.option('--output <dir>', `Folder for output artifacts (default: "test-results")`);
 program.option('--quiet', `Suppress stdio`);
 program.option('--repeat-each <N>', `Run each test N times (default: 1)`);
 program.option('--reporter <reporter>', `Reporter to use, comma-separated, can be ${builtinReporters.map(name => `"${name}"`).join(', ')} (default: "${defaultReporter}")`);
-program.option('--retries <retries>', `Maximum retry count for flaky tests (default: 0 for no retries)`);
+program.option('--retries <retries>', `Maximum retry count for flaky tests, zero for no retries (default: no retries)`);
 program.option('--shard <shard>', `Shard tests and execute only the selected shard, specify in the form "current/all", 1-based, for example "3/5"`);
 program.option('--project <project-name>', `Only run tests from the specified project (default: run all projects)`);
-program.option('--timeout <timeout>', `Specify test timeout threshold in milliseconds (default: ${defaultTimeout})`);
+program.option('--timeout <timeout>', `Specify test timeout threshold in milliseconds, zero for unlimited (default: ${defaultTimeout})`);
 program.option('-u, --update-snapshots', `Update snapshots with actual results (default: only create missing snapshots)`);
-program.option('-x', `Stop after the first failure (default: do not stop until all tests are run)`);
+program.option('-x', `Stop after the first failure`);
 program.version('Folio version ' + require('../package.json').version, '-v, --version', 'Output the version number');
 program.parse(process.argv);
 (async () => {
@@ -70,6 +71,13 @@ async function runTests(program: commander.Command) {
   const opts = program.opts();
   if (opts.help === undefined) {
     console.log(program.helpInformation());
+    console.log('');
+    console.log('Arguments [test-filter...]:');
+    console.log('  Pass arguments to filter test files. Each argument is treated as a regular expression.');
+    console.log('');
+    console.log('Examples:');
+    console.log('  $ folio my.spec.ts');
+    console.log('  $ folio -c tests/');
     process.exit(0);
   }
 
@@ -84,8 +92,6 @@ async function runTests(program: commander.Command) {
     }
     return false;
   }
-  const tsConfig = 'folio.config.ts';
-  const jsConfig = 'folio.config.js';
   if (opts.config) {
     const configFile = path.resolve(process.cwd(), opts.config);
     if (!fs.existsSync(configFile))
@@ -97,7 +103,7 @@ async function runTests(program: commander.Command) {
         runner.loadEmptyConfig(configFile);
     } else {
       // When passed a file, it must be a config file.
-      loadConfig(path.resolve(process.cwd(), opts.config));
+      loadConfig(configFile);
     }
   } else if (!loadConfig(path.resolve(process.cwd(), tsConfig)) && !loadConfig(path.resolve(process.cwd(), jsConfig))) {
     // No --config option, let's look for the config file in the current directory.
